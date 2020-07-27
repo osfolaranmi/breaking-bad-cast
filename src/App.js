454f0418f34f72cb3e86c7aa1ce36c8e7005dc4b
Simeon <fsimeon@simplexsystem.com>
2020-07-27 14:19:24 +0100

    Updated character component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/ui/Header';
+import CharacterGrids from './components/characters/CharacterGrids';
+import Search from './components/ui/Search.js';
+
 import './App.css';
 
-function App() {
+const App = () => {
+	const [items, setItems] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [query, setQuery] = useState('');
+
+	useEffect(() => {
+		const fetchItems = async () => {
+			const result = await axios(
+				`https://www.breakingbadapi.com/api/characters?name=${query}`
+			);
+			console.log(result.data);
+			setItems(result.data);
+			setIsLoading(false);
+		};
+
+		fetchItems();
+	}, [query]);
+
 	return (
-		<div className='App'>
-			<h1>Hello</h1>
+		<div className='container'>
+			<Header />
+			<Search getQuery={(q) => setQuery(q)} />
+			<CharacterGrids isLoading={isLoading} items={items} />
 		</div>
 	);
-}
+};
 
 export default App;
