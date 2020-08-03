561517dfcfa9cff8cac498fb5632d39114c2d1d1
osfolaranmi <osfolaranmi@gmail.com>
2020-08-03 08:30:20 +0100

    Updated gitignore

---
+++ build/precache-manifest.bd7f568033eb635aa312738a92b17ad7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9002956afef13d3a31bb59cc3a6b9e5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9fa138b049c2532a702",
+    "url": "/static/css/main.e862e97d.chunk.css"
+  },
+  {
+    "revision": "063a7d9a38da3de85d6e",
+    "url": "/static/js/2.813b259a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.813b259a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9fa138b049c2532a702",
+    "url": "/static/js/main.786fbda4.chunk.js"
+  },
+  {
+    "revision": "4aad21eb92ea52ff6838",
+    "url": "/static/js/runtime-main.9adee5b9.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
